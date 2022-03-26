a46aa4e32203566caaa2d629f2d6da2128fd1357
MdNadiruzzamanNibras <nadiruzzamannibras@gmail.com>
2022-03-26 09:34:38 +0600

    fetch compelet

--- src/Component/Shop/Shop.js
+++ src/Component/Shop/Shop.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Shop.css'
 const Shop = () => {
+    const [sunglasses, setSunglasses]=useState([])
+    useEffect(()=>{
+        fetch('data.json')
+        .then(res=> res.json())
+        .then(data=> console.log(data))
+    },[])
     return (
-        <div className='shop'>
+        <div className='shop-container'>
+            
+            <div className='products-container'>
+                
+            </div>
+            <div className='order-container'>
+               
+            </div>
             
         </div>
     );
