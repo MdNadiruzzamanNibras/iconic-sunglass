e0bb84227bcde337b762163f11f02fb2b9522596
MdNadiruzzamanNibras <nadiruzzamannibras@gmail.com>
2022-03-26 19:49:03 +0600

    first question

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <h1 className='fs-1'>iConic sunGlass</h1>
       <Shop></Shop>
       <h3>How  react works</h3>
-      <p>React is a library of javascript which build  user interfaces. It's use a special</p>
+      <p>React is a library of javascript which build  user interfaces. It's use a special syntax called JSX that allows the writing of HTML. You have to add all the components in app.js this can be seen in the UI. Here all component is created  with the function.React is to open in UI code npm start type in cmd or vs code terminal.</p>
     </div>
   );
 }
