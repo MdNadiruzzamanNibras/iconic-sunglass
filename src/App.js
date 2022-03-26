b424c3863c2665a086edf44af83eaab7bde1a8b8
MdNadiruzzamanNibras <nadiruzzamannibras@gmail.com>
2022-03-26 09:27:34 +0600

    Shop file create

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Shop from './Component/Shop/Shop';
 
 function App() {
   return (
     <div className="App">
-      
+      <Shop></Shop>
     </div>
   );
 }
---
+++ src/Component/Shop/Shop.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Shop.css'
+const Shop = () => {
+    return (
+        <div className='shop'>
+            
+        </div>
+    );
+};
+
+export default Shop;
